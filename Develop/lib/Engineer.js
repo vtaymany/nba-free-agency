ef8ede772e3428c27334059a52301776d6a28fb4
Van Taymany <vtaymany@gmail.com>
2020-12-15 16:27:47 -0800

    completes Employee.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// const Employee = require('./Employee')
+
+// class Engineer extends Emp {
+//   constructor(sideA, sideB) {
+//     const area = sideA * sideB
+//     const perimeter = sideA * 2 + sideB * 2
+
+//     super(area, perimeter)
+//     this.sideA = sideA
+//     this.sideB = sideB
+//   }
+// }
+
+// const rectangle = new Rectangle(12, 9)
+// rectangle.printInfo()
