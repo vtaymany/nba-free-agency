96b9d852165c4a815c6e3711d940fdfbc2578b01
Van Taymany <vtaymany@gmail.com>
2020-12-15 16:48:58 -0800

    adds comments to Engineer.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,15 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Employee = require('./Employee')
 
+//Extends on employee class
 class Engineer extends Employee {
   constructor(name, id, email, github) {
     super(name, id, email)
     this.github = github
   }
+  //Returns engineer's Github
   getGithub() {
     return this.github
   }
-  //Returns employee's role
+  //Returns engineers's role
   getRole() {
     return 'Engineer'
   }
